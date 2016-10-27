b61e1623dfd7aa4fa71058b930e20fe3e58bea5a
jnst <jnst@outlook.com>
2016-10-28 03:58:04 +0900

    Add redis

---
+++ redis/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	redis "gopkg.in/redis.v5"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+}
