1fb740945bf2a77c5ee031bd8385a34fbf8f74c8
jnst <jnst@outlook.com>
2017-02-23 02:02:37 +0900

    Add interface sample

---
+++ interface.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type I interface {
+	M() string
+}
+
+type T struct {
+	name string
+	fmt.Stringer
+}
+
+func (t T) M() string {
+	return t.name
+}
+
+func (t T) String() string {
+	return fmt.Sprintf("hello %s", t.name)
+}
+
+func Hello(i I) {
+	fmt.Printf("hello %s\n", i.M())
+}
+
+func main() {
+	Hello(T{name: "world"})
+	fmt.Println(T{name: "world"}.String())
+}
